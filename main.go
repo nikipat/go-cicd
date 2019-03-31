e408e9ef0f6f41888c44429233203a7a9e12e4a7
Prabhat Sharma <hi.prabhat@gmail.com>
2019-03-31 13:34:01 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// HandlerFunc Sample documentation for HandlerFunc
+func HandlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "\nRelease 1")
+}
+
+func main() {
+	http.HandleFunc("/", HandlerFunc)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
