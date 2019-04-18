867fd85c48285a39247fd1d2d881cc88d300183e
Prabhat Sharma <hi.prabhat@gmail.com>
2019-04-18 08:52:48 -0700

    release

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 // HandlerFunc1 Sample documentation for HandlerFunc
 func HandlerFunc1(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "\nRelease 2")
+	fmt.Fprintf(w, "\nRelease 3")
 }
 
 // //Calculate just calculates
