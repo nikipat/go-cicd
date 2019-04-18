56a2d5f13e637f9b91a36df7fa807cf860bc77a2
Prabhat Sharma <hi.prabhat@gmail.com>
2019-04-17 18:51:15 -0700

    unit test added

--- main.go
+++ main.go
@@ -6,13 +6,18 @@ import (
 	"net/http"
 )
 
-// HandlerFunc Sample documentation for HandlerFunc
-func HandlerFunc(w http.ResponseWriter, r *http.Request) {
+// HandlerFunc1 Sample documentation for HandlerFunc
+func HandlerFunc1(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "\nRelease 1")
 	fmt.Fprintf(w, "\nRelease 2")
 }
 
+// //Calculate just calculates
+// func Calculate(val int)(result int){
+// 	return val*2
+// }
+
 func main() {
-	http.HandleFunc("/", HandlerFunc)
+	http.HandleFunc("/", HandlerFunc1)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
---
+++ main_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+//TestServer will test if server returns correct values
+func TestHandlerFunc1(t *testing.T) {
+
+	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+	// pass 'nil' as the third parameter.
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(HandlerFunc1)
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+
+// func TestCalculate(t *testing.T) {
+// 	if Calculate(2) != 4 {
+// 		t.Error("Expected 4 but failed")
+// 	}
+// }
