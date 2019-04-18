3bf9f49b46a5548f0cbd6d7433c70715fa04707e
Prabhat Sharma <hi.prabhat@gmail.com>
2019-04-17 23:34:34 -0700

    release 4

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func HandlerFunc1(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "\nRelease 1")
 	fmt.Fprintf(w, "\nRelease 2 ")
 	fmt.Fprintf(w, "\nRelease 3.1 ")
+	fmt.Fprintf(w, "\nRelease 4")
 }
 
 // //Calculate just calculates
